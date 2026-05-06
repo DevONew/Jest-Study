48c27e8182eca8a655510a472d019716bca18394
DevONew <kyl2778@naver.com>
2026-05-06 13:42:43 +0900

    jest 학습 mock()을 이용한 모듈 심화

--- apiClient.test.js
+++ apiClient.test.js
@@ -1,35 +1,148 @@
 // apiClient.test.js
 const { fetchData } = require("./apiClient");
+const axios = require("axios");
+
+jest.mock("axios");
 
 describe("apiClient.js 테스트", () => {
-  test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+  let callback;
 
-    // url 전에 반드시 먼저 작성 할 것. 
-    // global 객체는 node 환경에서의 최상위 객체 
-    // Node 18부터 fetch가 전역으로 내장 되어서 global.fetch로 안써도 됨. 
-    //global.fetch = jest.fn(); 일경우 json이 undefined하다는 결과가 나옴
-    global.fetch = jest.fn().mockResolvedValue(
-        {
-            json: jest.fn().mockResolvedValue({
-                id: 1, 
-                name: "MARTIN",
-                address: {
-                    street: "Kulas Light",
-                    suite: "Apt. 556",
-                    city: "Gwenborough",
-                }
-            })
-        }
-    );
+  beforeEach(() => {
+    callback = jest.fn();
 
-    const url = "https://jsonplaceholder.typicode.com/users/1";
-
-    const result = await fetchData(url);
-
-    expect(result).toEqual({
-      userId: 1,
-      formattedName: "MARTIN",
-      address: "Kulas Light Apt. 556 Gwenborough",
+    axios.get.mockResolvedValue({
+      data: {
+        id: 1,
+        name: "김철수",
+        address: {
+          street: "테스트 거리",
+          suite: "테스트 호수",
+          city: "서울",
+        },
+      },
     });
   });
+
+  test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+    // Arrange
+    const url = "https://api.example.com/user/1";
+
+    // Act
+    const result = await fetchData(url);
+
+    // Assert
+    expect(result).toEqual({
+      userId: 1,
+      formattedName: "김철수",
+      address: "테스트 거리 테스트 호수 서울",
+    });
+  });
+
+  test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+    // Arrange
+    const url = "https://api.example.com/user/1";
+
+    // Act
+    const result = await fetchData(url);
+
+    // Assert
+    expect(result).toEqual({
+      userId: 1,
+      formattedName: "김철수",
+      address: "테스트 거리 테스트 호수 서울",
+    });
+  });
+
+  test("callback 함수가 제공되면 호출되는지 확인", async () => {
+    // Arrange
+    // 여기서도 URL을 자유롭게 수정해도 괜찮습니다.
+    const url = "https://api.example.com/user/1";
+    // const callback = jest.fn();
+
+    // Act
+    await fetchData(url, callback);
+
+    // Assert
+    // 가짜 함수인 callback이 호출되었는지 확인
+    expect(callback).toHaveBeenCalled();
+  });
+
+  test("callback 함수가 포멧된 데이터를 인자로 가지고 호출되는지 확인", async () => {
+    // Arrange
+    const url = "https://api.example.com/user/1";
+    // const callback = jest.fn();
+
+    // Act
+    await fetchData(url, callback);
+
+    // Assert
+    // toHaveBeenCalled는 호출 여부만 확인
+    // toHaveBeenCalledWith는 호출된 인자를 확인
+    expect(callback).toHaveBeenCalledWith({
+      userId: 1,
+      formattedName: "김철수",
+      address: "테스트 거리 테스트 호수 서울",
+    });
+  });
+
+  test("callback 함수가 한 번 호출되는지 확인", async () => {
+    // Arrange
+    const url = "https://api.example.com/user/1";
+    // const callback = jest.fn();
+
+    // Act
+    await fetchData(url, callback);
+
+    // Assert
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  test("callback이 제공되지 않은 경우 callback 함수가 호출되지 않는지 확인", async () => {
+    // Arrange
+    const url = "https://api.example.com/user/1";
+    // const callback = jest.fn();
+
+    // Act
+    // callback 함수 전달 X
+    await fetchData(url);
+
+    // Assert
+    // 0번 호출되었는지 확인
+    expect(callback).toHaveBeenCalledTimes(0);
+    // 또는 호출이 되지 않았는지 확인
+    expect(callback).not.toHaveBeenCalled();
+  });
 });
+
+// describe("apiClient.js 테스트", () => {
+//   test("API 호출 후 데이터 포맷이 올바르게 되는지 확인", async () => {
+
+//     // url 전에 반드시 먼저 작성 할 것. 
+//     // global 객체는 node 환경에서의 최상위 객체 
+//     // Node 18부터 fetch가 전역으로 내장 되어서 global.fetch로 안써도 됨. 
+//     //global.fetch = jest.fn(); 일경우 json이 undefined하다는 결과가 나옴
+//     global.fetch = jest.fn().mockResolvedValue(
+//         {
+//             json: jest.fn().mockResolvedValue({
+//                 id: 1, 
+//                 name: "MARTIN",
+//                 address: {
+//                     street: "Kulas Light",
+//                     suite: "Apt. 556",
+//                     city: "Gwenborough",
+//                 }
+//             })
+//         }
+//     );
+
+//     const url = "https://jsonplaceholder.typicode.com/users/1";
+
+//     const result = await fetchData(url);
+
+//     expect(result).toEqual({
+//       userId: 1,
+//       formattedName: "MARTIN",
+//       address: "Kulas Light Apt. 556 Gwenborough",
+//     });
+//   });
+// });
